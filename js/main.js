8bdb8a4d7b712b5a4ef6bfb7d2497d5bbd0dc447
Braxton Toomer <braxton.toomer1@gmail.com>
2020-06-03 15:47:33 -0700

    Creates factory functions and modules

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@
 		1). Use minimal global code as possible.
 		2). Utilize Factory and/or Module Functions
 		3). Separation of Concerns
+
+		Rule of Thumb:
+		1). If you need one of something - use a module.
+		2). If you need multiple - use a factory.
 */
 
 //	Declare Variables
@@ -19,4 +23,30 @@
 
 
 //	Functions
+
+
+//	Create Factory Function for Player
+const player = (name, symbol) => {
+	return { name, symbol };
+};
+
+
+//	Create Module for Game Board
+const gameBoard = (function () {
+	//board, reset, sounds
+	let board = [];
+	return {
+		board,
+	};
+})();
+
+
+//	Create Module for Game Logic
+const gameLogic = (function () {
+	//winner, turn, tie
+	return {
+
+	};
+})();
+
 
