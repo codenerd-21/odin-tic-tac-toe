f2f657f305cf22a35191cadc1f85bc9f3442460a
Braxton Toomer <braxton.toomer1@gmail.com>
2020-06-03 15:13:34 -0700

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/*	Browser Based Tic Tac Toe Game
+
+	  Focus:
+		1). Use minimal global code as possible.
+		2). Utilize Factory and/or Module Functions
+		3). Separation of Concerns
+*/
+
+//	Declare Variables
+
+
+//	Declare Constants
+
+
+//	Declare DOM Cached Elements
+
+
+//	Event Listeners
+
+
+//	Functions
+
